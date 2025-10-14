488d6b78a7925cd096fc774795a7c6668a94ab15
Alejandro4092 <acobomarcos@gmail.com>
2025-10-14 13:20:39 +0200

    Dibujos dinamicos con push

--- src/components/DibujosComplejos.js
+++ src/components/DibujosComplejos.js
@@ -1,18 +1,25 @@
 import { Component } from "react";
 class DibujosComplejos extends Component {
+    dibujarNumeros=()=>{
+        var lista=[];
+        for(var i=1;i<=7;i++){
+            var numero= parseInt(Math.random()*100)+1;
+            //MEDIANTE PUSH VAMOS AÑADIENDO METODOS A LA LISTA
+            lista.push(<li key={i}>{numero}</li>)
 
-
-
-
-
-
-
-
+        }
+        return lista;
+    }
 
     render() {
         return (
             <div>
                 <h1>Dibujos dinámicos React</h1>
+                <ul>
+                    {this.dibujarNumeros()}
+
+
+                </ul>
 
 
             </div>
